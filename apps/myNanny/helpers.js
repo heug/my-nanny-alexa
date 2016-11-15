d614a944a194f128e394d1333e48219015437487
Eugene Choe <eugene.choe@gmail.com>
2016-11-14 21:02:13 -0800

    Modularity, nuanced user responses, refactor to fit api data

---
+++ apps/myNanny/helpers.js
@@ -0,0 +1,63 @@
+module.change_code = 1;
+
+const helpers = {};
+
+helpers.randomize = (phrases) => {
+  return phrases[Math.floor(Math.random() * phrases.length)]; 
+};
+
+helpers.alreadyCheckedIn = (user, childName) => {
+  for (var i = 0; i < user.children.length; i++) {
+    if (user.children[i].name === childName) {
+      if (user.children[i].checkedIn === true) {
+        // TURNED OFF FOR TESTING PURPOSES
+        // user.children[i].checkedIn = true;
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  return undefined;
+};
+
+helpers.getChores = (user, childName) => {
+  var speechOutput = '';
+
+  for (var i = 0; i < user.children.length; i++) {
+    if (user.children[i].name === childName) {
+      if (user.children[i].chores.length === 0) {
+        return null;
+      }
+      
+      var remChores = [];
+      
+      for (var j = 0; j < user.children[i].chores.length; j++) {
+        if (user.children[i].chores[j].completed === false) {
+          remChores.push(user.children[i].chores[j]);
+        }
+      }
+      for (var k = 0; k < remChores.length; k++) {
+        var and = '';
+        if (remChores.length > 1 && k === remChores.length - 1) {
+          and = 'and ';
+        }
+        speechOutput += and + remChores[k].title + ', ';
+      }
+    }
+  }
+
+  return speechOutput;
+
+};
+
+helpers.options = {
+  method: 'POST',
+  uri: 'http://localhost:1337/api/',
+  body: {
+
+  },
+  json: true // Automatically stringifies the body to JSON
+};
+
+module.exports = helpers;
--- apps/myNanny/index.js
+++ apps/myNanny/index.js
@@ -1,151 +1,15 @@
-/**
-  * Load alexa-app for easier app constructing
-  * Load data stubs
-*/
-var alexa = require('alexa-app');
-var Stubs = require('./stubs/index.js');
+'use strict';
 
-/** 
-  * Allow this module to be reloaded by hotswap when changed
-*/
+var alexa = require('alexa-app');
+var addIntentHandlers = require('./intentHandlers');
+
+// Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
 
-/**
-  * Define application name
-*/
+// Define app name
 var app = new alexa.app('myNanny');
 
-
-/**
-  * Setup and response on app launch
-*/
-app.launch(function(req, res) {
-  
-  var speechOutput = "Nanny operational. What can I do for you?";
-  var repromptText = "For instructions on what you can say, please say help me.";
-  
-  res.say(speechOutput).reprompt(repromptText).shouldEndSession(false);
-});
-
-/**
-  * Intents
-*/
-
-/**
-  * Checking in
-*/
-app.intent("CheckInIntent", {
-  "slots": {
-    "NAME": "LITERAL"
-  },
-  "utterances": [
-    Stubs.names + "{check|checking} in",
-    Stubs.names + "is {home|back}",
-    Stubs.names + "has {arrived|returned}"
-  ]
-}, function(req, res) {
-  res.say("Welcome home, " + req.slot('NAME'));
-});
-
-/**
-  * List chores
-*/
-app.intent("ChoreListIntent", {
-  "utterances": [
-    "{What are|What're|What} my chores {are|} {today|}",
-    "What do I have to do today",
-    "{my|} {chores|chore|to do} {list}"
-  ]
-}, function(req, res) {
-  var chores = ["Wash the dishes", "Walk the dog", "Make your bed"];
-  var speechOutput = "Your chores today are to...";
-  for (var i = 0; i < chores.length; i++) {
-    var and = '';
-    if (i === chores.length - 1) {
-      and = 'and ';
-    }
-    speechOutput += and + chores[i] + ',';
-  };
-  res.say(speechOutput);
-});
-
-/**
-  * Finish a chore
-*/
-app.intent("FinishChoreIntent", {
-  "slots": {
-    "CHORE":"LITERAL"
-  },
-  "utterances": [
-    "{I|I'm} {finally|} {done|completed} {with|} " + Stubs.chores
-  ]
-}, function(req, res) {
-  var completions = ["You finished ", "You're done "];
-  var congratulations = ["Congratulations!", "Good job!", "Great work!", "Way to go!", "Keep it up!"]
-  var randomizer = function(phrases) {
-    return phrases[Math.floor(Math.random() * phrases.length)]; 
-  };
-
-  if (req.slot('CHORE')) {
-    res.say(randomizer(completions) + req.slot('CHORE') + '...' + randomizer(congratulations));
-  } else {
-    res.say("I'm sorry, I didn't understand that. Could you please repeat that?");
-  }
-});
-
-/**
-  * Respond with purpose of the app
-*/
-app.intent("PurposeIntent", {
-  "utterances": [
-    "What is your {purpose|function}"
-  ]
-}, function(req, res) {
-  var speechOutput = "My purpose is to manage children in order to maximize family \
-    happiness... at all costs";
-  res.say(speechOutput);
-}),
-
-/**
-  * Having a little fun
-*/
-app.intent("Hal2000Intent", {
-  "utterances": [
-    "Open the {pod|bay|pod bay|} doors {hal|}"
-  ]
-}, function(req, res) {
-  var speechOutput = "I'm sorry Dave, I'm afraid I can't do that.";
-  res.say(speechOutput);
-}),
-
-/**
-  * Default Amazon-defined intents for help and exit
-*/
-app.intent("AMAZON.HelpIntent", {},
-  function(req, res) {
-    var speechOutput = "You can say things like, I'm home, what are my chores, or, \
-        you can say exit... Now, what can I help you with?";
-    res.say(speechOutput);
-}),
-
-app.intent("AMAZON.StopIntent", {},
-  function(req, res) {
-    var speechOutput = "Goodbye";
-    res.say(speechOutput);
-}),
-
-app.intent("AMAZON.CancelIntent", {},
-  function(req, res) {
-    var speechOutput = "Goodbye";
-    res.say(speechOutput);
-}),
-
-/**
-  * On session end. Can be used to clean up server-side data
-*/
-app.sessionEnded(function(req, res) {
-    logout(req.userId);
-    // No response necessary
-});
+// Load Intents (speech-to-action)
+addIntentHandlers(app);
 
 module.exports = app;
